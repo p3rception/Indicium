4d02db13bb633ee4ed898c2fb19e10b5dfb126fc
dimitris-per <dim.pergel@gmail.com>
2023-05-15 00:58:09 +0300

    Added thresholds instead of ints

--- battery.py
+++ battery.py
@@ -2,6 +2,10 @@ import subprocess
 import time
 import os
 
+LOW_BATTERY_THRESHOLD = 20
+HIGH_BATTERY_THRESHOLD = 80
+
+
 def check_battery():
     """
     Returns the current battery level of the device
@@ -10,25 +14,28 @@ def check_battery():
         "pmset -g batt | grep -Eo '[0-9]{1,3}%' | awk '{print $1}'", shell=True).decode("utf-8").strip().split("%")[0])
     return battery_level
 
+
 def is_power_connected():
     """
     Returns True if the device is currently charging, else returns False
     """
-    power_status = subprocess.check_output("pmset -g ps", shell=True).decode("utf-8").strip()
+    power_status = subprocess.check_output(
+        "pmset -g ps", shell=True).decode("utf-8").strip()
     if "AC Power" in power_status:
         return True
     else:
         return False
+
 
 # The main loop continuously checks the battery level and sends notifications based on the battery status
 while True:
     battery_level = check_battery()
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(f"[{timestamp}] The battery level is {battery_level}%")
-    if battery_level <= 20:
+    if battery_level <= LOW_BATTERY_THRESHOLD and not is_power_connected():
         os.system(
             "osascript -e 'display notification \"Battery Level Below 20%\nCharge the device!\" with title \"Battery Status\"'")
-    elif battery_level >= 80 and is_power_connected():
+    elif battery_level >= HIGH_BATTERY_THRESHOLD and is_power_connected():
         os.system(
             "osascript -e 'display notification \"Battery Level Above 80%\nUnplug the device!\" with title \"Battery Status\"'")
-    time.sleep(600) # Checks every 10 minutes
+    time.sleep(600)  # Checks every 10 minutes
